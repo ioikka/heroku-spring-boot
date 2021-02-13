bfb1db570a944d4fe514392c9af7f75a229258bb
ioikka <belgampaul@gmail.com>
2021-02-13 18:10:30 +0300

    Initial commit

---
+++ src/main/java/io/ikka/demo/herokuspringboot/HerokuSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.ikka.demo.herokuspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ikka/demo/herokuspringboot/HerokuSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ikka.demo.herokuspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
